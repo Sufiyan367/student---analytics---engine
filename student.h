77d93eaf1418306feccd56d91553ff29d4e6ba0d
Sufiyan367 <sufiyan.surve@indiraicem.ac.in>
2026-01-12 16:43:50 +0530

    Add files via upload

---
+++ student.h
@@ -0,0 +1,14 @@
+#ifndef student_h
+#define student_h
+
+typedef struct
+{
+    int roll;
+    int attendence;
+    int midsem;
+    int tae1;
+    int tae2;
+    int finalexam;
+}student;
+
+#endif
